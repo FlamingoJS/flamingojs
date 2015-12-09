e7c13141ee3b1ae837df9385ee514f282c81aa3f
Bruno Queiros <brunoofq@gmail.com>
2015-12-08 23:56:46 -0200

    update test

--- test/index.js
+++ test/index.js
@@ -2,8 +2,6 @@ import 'babel-core/register';
 
 import test from 'ava';
 import jsdom from 'jsdom';
-import isLength from '../lib/validators/isLength';
-import isEmail from '../lib/validators/isEmail';
 import isRequired from '../lib/validators/isRequired';
 import FlamingoJS from '../lib/index';
 
@@ -32,11 +30,9 @@ test('should add the class .error-field', t => {
   const fields = [
     {
       'element': '#email',
-      'error-element': '.remember',
       'rules': [
         {
-          'module': isLength,
-          'options': { 'min': 2, 'max': 9 }
+          'validator': isRequired
         }
       ]
     }
@@ -45,68 +41,13 @@ test('should add the class .error-field', t => {
   let input = document.createElement('input');
   input.setAttribute('id', 'email');
   input.setAttribute('type', 'email');
-  input.setAttribute('value', 'Hello world!');
+  input.setAttribute('value', '');
   document.body.appendChild(input);
 
-  flamingo.validate(fields);
-
-  t.true(document.querySelector('#email').classList.contains('error-field'));
-  document.body.removeChild(input);
-});
-
-test('should add the class .error-field', t => {
-  const fields = [
-    {
-      'element': '#email',
-      'error-element': '.remember',
-      'rules': [
-        {
-          'module': isRequired
-        },
-        {
-          'module': isLength,
-          'options': { 'min': 2, 'max': 30 }
-        },
-        {
-          'module': isEmail
-        }
-      ]
-    }
-  ];
-
-  let input = document.createElement('input');
-  input.setAttribute('id', 'email');
-  input.setAttribute('type', 'email');
-  input.setAttribute('value', 'myemail@providercom');
-  document.body.appendChild(input);
-
-  flamingo.validate(fields);
-
-  t.true(document.querySelector('#email').classList.contains('error-field'));
-  document.body.removeChild(input);
-});
-
-test('should not add the class .error-field', t => {
-  const fields = [
-    {
-      'element': '#email',
-      'error-element': '.remember',
-      'rules': [
-        {
-          'module': isLength,
-          'options': { 'min': 2, 'max': 20 }
-        }
-      ]
-    }
-  ];
-
-  let input = document.createElement('input');
-  input.setAttribute('id', 'email');
-  input.setAttribute('type', 'email');
-  input.setAttribute('value', 'Hello world');
-  document.body.appendChild(input);
-
-  flamingo.validate(fields);
-
-  t.false(document.querySelector('#email').classList.contains('error-field'));
+  flamingo.validate(fields)
+    .then((response) => {
+      t.true(document.querySelector('#email').classList.contains('error-field'));
+      t.same(document.querySelector('.error-message').innerHTML, 'This field is required.');
+      document.body.removeChild(input);
+    });
 });
