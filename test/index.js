e09a6f3e921648c2802aa12f59d2362defbeefbb
Bruno Queiros <brunoofq@gmail.com>
2015-12-11 17:03:53 -0200

    update index.js test

--- test/index.js
+++ test/index.js
@@ -14,6 +14,10 @@ describe('FlamingoJS', () => {
     flamingo = new FlamingoJS();
   });
 
+  afterEach(() => {
+    document.body.innerHTML = '';
+  });
+
   it('should add the class .error-field', (done) => {
     const fields = [
       {
@@ -44,11 +48,51 @@ describe('FlamingoJS', () => {
     dom.appendChild(document.body, input);
 
     flamingo.validate(fields)
-      .then(() => {
+      .then(() => {}, (errors) => {
+        const expectedError = { element: '#email', rule: 'isRequired' };
+
+        expect(Object.keys(errors).length).to.equal(1);
+        expect(JSON.stringify(errors['#email'])).to.equal(JSON.stringify(expectedError));
+        expect(dom.hasClass('#email', 'error-field')).to.be.true;
+        expect(dom.hasClass('[for="email"]', 'error-label')).to.be.true;
         expect(dom.getElement('.error-message').innerHTML).to.equal('This field is required.');
 
-        dom.remove(input);
         done();
-      }, done);
+      });
+  });
+
+  it('should not add the .error-field class', (done) => {
+    const fields = [
+      {
+        'element': '#email',
+        'rules': [
+          {
+            'validator': isRequired
+          }
+        ]
+      }
+    ];
+
+    const label = dom.create('label', {
+      'innerHTML': 'Email',
+      'attributes': {
+        'for': 'email'
+      }
+    });
+    dom.appendChild(document.body, label);
+
+    const input = dom.create('input', {
+      'attributes': {
+        'id': 'email',
+        'type': 'email',
+        'value': 'brunoofq@gmail.com'
+      }
+    });
+    dom.appendChild(document.body, input);
+
+    flamingo.validate(fields)
+      .then(() => {
+        done();
+      });
   });
 });
